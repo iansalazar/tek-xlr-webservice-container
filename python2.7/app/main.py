6f7cd91e08ae2c02493ff636350e4dab7a8d2ce4
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-22 11:00:40 -0500

    Adding: user story, defect, feature, security and test status endpoints

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -9,10 +9,39 @@ def epicstatus():
     json_str = json.dumps(dict)
     return json_str
 
+@app.route("/storystatus")
+def storystatus():
+    dict = {'green': 58, 'red': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/defectstatus")
+def defectstatus():
+    dict = {'closed': 2, 'open': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/featurestatus")
+def featurestatus():
+    dict = {'green': 19, 'red': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/securitystatus")
+def securitystatus():
+    dict = {'issues': 1, 'critical': 0}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/testcasestatus")
+def testcasestatus():
+    dict = {'passed': 53, 'failed': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
 @app.route("/")
 def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 2.7 (default)"
+    return "Hello from the TEK XLR Web service...."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
