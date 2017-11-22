a6980c6a6b30f5698725967f58917d0984de7e9c
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-22 14:59:05 -0500

    Adding: epic status detail endpoint

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -9,6 +9,16 @@ def epicstatus():
     json_str = json.dumps(dict)
     return json_str
 
+@app.route("/epicstatusdetail")
+def epicstatusdetail():
+    details = [{"name":"DE1","url":"http://www.google.com"},{"owner":"J. Doe","status":"Green"},{"test-start":"10/01/17","test-cases-assigned": 4,"executed": 4, "percent-compete": 100, 
+                "passed": 4, "failed": 4, "defects-logged": 1, "open": 0, "resolved": 0, "closed": 1 },
+               {"name":"DE2","url":"http://www.google.com"},{"owner":"J. Doe","status":"Green"},{"test-start":"10/01/17","test-cases-assigned": 9,"executed": 9, "percent-compete": 100, 
+                "passed": 9, "failed": 0, "defects-logged": 0, "open": 0, "resolved": 0, "closed": 0 }
+                ]
+    json_str = json.dumps(details)
+    return json_str
+
 @app.route("/storystatus")
 def storystatus():
     dict = {'green': 58, 'red': 1}
