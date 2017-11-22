e81c8f115f9073de4c47e785bbfdf0448c4a35e6
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-22 17:33:36 -0500

    Modifying: epic status detail endpoint

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route("/epicstatus")
 def epicstatusdetail():
-    details = [{"name":"DE1","url":"http://www.google.com"},{"owner":"J. Doe","status":"Green"},{"test-start":"10/01/17","test-cases-assigned": 4,"executed": 4, "percent-compete": 100, 
-                "passed": 4, "failed": 4, "defects-logged": 1, "open": 0, "resolved": 0, "closed": 1 },
-               {"name":"DE2","url":"http://www.google.com"},{"owner":"J. Doe","status":"Green"},{"test-start":"10/01/17","test-cases-assigned": 9,"executed": 9, "percent-compete": 100, 
-                "passed": 9, "failed": 0, "defects-logged": 0, "open": 0, "resolved": 0, "closed": 0 }
+    details = [{"name":"DE1", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green" , "test-start":"10/01/17", "test-cases-assigned": 4, "executed": 4, "percent-compete": 100, 
+                "passed": 4, "failed": 4, "defects-logged": 1, "open": 0, "resolved": 0, "closed": 1},
+               {"name":"DE2", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "test-start":"10/01/17", "test-cases-assigned": 9, "executed": 9, "percent-compete": 100, 
+                "passed": 9, "failed": 0, "defects-logged": 0, "open": 0, "resolved": 0, "closed": 0}
                 ]
     json_str = json.dumps(details)
     return json_str
