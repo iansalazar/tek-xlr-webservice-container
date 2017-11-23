e028be1be85253ed58bf5b0fe1e886f55bedbdc5
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-23 00:29:06 -0500

    working on dynamic data for user story status and detail endpoint

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -4,7 +4,7 @@ import json
 app = Flask(__name__)
 
 @app.route("/epicstatus")
-def epicstatusdetail():
+def epicstatus():
     details = [
 		{"name":"DE1", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green" , "teststart":"10/01/17", "testcasesassigned": 4, "executed": 4, "percentcomplete": 100, 
                  "passed": 4, "failed": 0, "defectslogged": 1, "open": 0, "resolved": 0, "closed": 1},
@@ -30,8 +30,33 @@ def epicstatusdetail():
 
 @app.route("/storystatus")
 def storystatus():
-    dict = {'green': 58, 'red': 1}
-    json_str = json.dumps(dict)
+    details = [
+                {"name":"Story1", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green" , "teststart":"10/01/17", "testcasesassigned": 4, "executed": 4, "percentcomplete": 100,
+                 "passed": 4, "failed": 0, "defectslogged": 1, "open": 0, "resolved": 0, "closed": 1},
+                {"name":"Story2", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 9, "executed": 9, "percentcomplete": 100,
+                 "passed": 9, "failed": 0, "defectslogged": 0, "open": 0, "resolved": 0, "closed": 0},
+                {"name":"Story3", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 1, "executed": 1, "percentcomplete": 100,
+                 "passed": 1, "failed": 0, "defectslogged": 1, "open": 0, "resolved": 0, "closed": 1},
+                {"name":"Story4", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 4, "executed": 4, "percentcomplete": 100,
+                 "passed": 4, "failed": 0, "defectslogged": 0, "open": 0, "resolved": 0, "closed": 0},
+                {"name":"Story5", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 2, "executed": 2, "percentcomplete": 100,
+                 "passed": 9, "failed": 2, "defectslogged": 0, "open": 0, "resolved": 0, "closed": 0},
+                {"name":"Story6", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 6, "executed": 6, "percentcomplete": 100,
+                 "passed": 6, "failed": 0, "defectslogged": 0, "open": 0, "resolved": 0, "closed": 0},
+                {"name":"Story7", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 14, "executed": 14, "percentcomplete": 100,
+                 "passed": 14, "failed": 0, "defectslogged": 0, "open": 0, "resolved": 0, "closed": 0},
+                {"name":"Story8", "url":"http://www.google.com", "owner":"J. Doe", "status":"Green", "teststart":"10/01/17", "testcasesassigned": 10, "executed": 10, "percentcomplete": 100,
+                 "passed": 10, "failed": 0, "defectslogged": 0, "open": 0, "resolved": 0, "closed": 0},
+                {"name":"Story9", "url":"http://www.google.com", "owner":"J. Doe", "status":"Red", "teststart":"11/15/17", "testcasesassigned": 4, "executed": 4, "percentcomplete": 100,
+                 "passed": 3, "failed": 1, "defectslogged": 1, "open": 1, "resolved": 0, "closed": 0},
+                {"name":"Story10", "url":"http://www.google.com", "owner":"J. Doe", "status":"Red", "teststart":"11/15/17", "testcasesassigned": 6, "executed": 6, "percentcomplete": 100,
+                 "passed": 3, "failed": 3, "defectslogged": 3, "open": 3, "resolved": 0, "closed": 0},
+                {"name":"Story11", "url":"http://www.google.com", "owner":"J. Doe", "status":"Red", "teststart":"11/15/17", "testcasesassigned": 4, "executed": 4, "percentcomplete": 100,
+                 "passed": 3, "failed": 1, "defectslogged": 1, "open": 1, "resolved": 0, "closed": 0},
+                {"name":"Story12", "url":"http://www.google.com", "owner":"J. Doe", "status":"Red", "teststart":"11/15/17", "testcasesassigned": 8, "executed": 8, "percentcomplete": 100,
+                 "passed": 4, "failed": 4, "defectslogged": 4, "open": 4, "resolved": 0, "closed": 0}
+              ]
+    json_str = json.dumps(details)
     return json_str
 
 @app.route("/defectstatus")
