dd98d5d13df097494b85e404bdfee0f4c725bb7a
Ian Salazar <iansalazar@Ians-MacBook-Pro.local>
2017-11-24 14:27:10 -0500

    worked on latests builds tile

--- python2.7/app/main.py
+++ python2.7/app/main.py
@@ -61,8 +61,19 @@ def storystatus():
 
 @app.route("/defectstatus")
 def defectstatus():
-    dict = {'closed': 2, 'open': 1}
-    json_str = json.dumps(dict)
+    details = [
+                {"defect":"Defect1", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect2", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect3", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect4", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect5", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect6", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect7", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect8", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Green" , "workstart":"10/01/17", "state": 1, "percentcomplete": 100},
+		{"defect":"Defect9", "url":"http://www.google.com", "owner":"J. Doe", "description":"Description..." , "status":"Red" , "workstart":"11/05/17", "state": 1, "percentcomplete": 100},
+              ] 
+
+    json_str = json.dumps(details)
     return json_str
 
 @app.route("/featurestatus")
@@ -97,6 +108,59 @@ def securitystatus():
 def testcasestatus():
     dict = {'passed': 53, 'failed': 1}
     json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/unitteststatus")
+def unitteststatus():
+    dict = {'passed': 53, 'failed': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/codeanalysisstatus")
+def codeanalysisstatus():
+    dict = {'passed': 53, 'failed': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/syntheticteststatus")
+def syntheticteststatus():
+    dict = {'passed': 53, 'failed': 1}
+    json_str = json.dumps(dict)
+    return json_str
+
+@app.route("/builds")
+def builds():
+    details = [
+                {"number":2200, "state": 1, "branch": "master", "jobstart": "2017-11-22T11:30:00Z", "jobend": "2017-11-22T11:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2199, "state": 1, "branch": "master", "jobstart": "2017-11-22T10:30:00Z", "jobend": "2017-11-22T10:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2198, "state": 1, "branch": "master", "jobstart": "2017-11-22T09:30:00Z", "jobend": "2017-11-22T09:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2197, "state": 1, "branch": "master", "jobstart": "2017-11-22T08:30:00Z", "jobend": "2017-11-22T08:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2196, "state": 1, "branch": "master", "jobstart": "2017-11-22T07:30:00Z", "jobend": "2017-11-22T07:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2195, "state": 0, "branch": "master", "jobstart": "2017-11-22T06:30:00Z", "jobend": "2017-11-22T06:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2194, "state": 1, "branch": "master", "jobstart": "2017-11-22T05:30:00Z", "jobend": "2017-11-22T05:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2193, "state": 1, "branch": "master", "jobstart": "2017-11-22T04:30:00Z", "jobend": "2017-11-22T04:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2192, "state": 1, "branch": "master", "jobstart": "2017-11-22T03:30:00Z", "jobend": "2017-11-22T03:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2191, "state": 1, "branch": "master", "jobstart": "2017-11-22T02:30:00Z", "jobend": "2017-11-22T02:35:00Z", "commitmessage": "Changed a lot of things.." },
+		{"number":2190, "state": 0, "branch": "master", "jobstart": "2017-11-22T01:30:00Z", "jobend": "2017-11-22T01:35:00Z", "commitmessage": "Changed a lot of things.." }
+              ]
+
+    json_str = json.dumps(details)
+    return json_str
+
+@app.route("/deployments")
+def deployments():
+    details = [
+                {"description":"Feature 1 Description", "stories": 4},
+                {"description":"Feature 2 Description", "stories": 1},
+                {"description":"Feature 3 Description", "stories": 2},
+                {"description":"Feature 4 Description", "stories": 7},
+                {"description":"Feature 5 Description", "stories": 3},
+                {"description":"Feature 6 Description", "stories": 8},
+                {"description":"Feature 7 Description", "stories": 5},
+                {"description":"Feature 8 Description", "stories": 9}
+              ]
+
+    json_str = json.dumps(details)
     return json_str
 
 @app.route("/")
